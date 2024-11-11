a23ee8dcdc13d23a37dabb01fec642e9c4dfb789
Cata <127821154+catalinaAlba@users.noreply.github.com>
2024-11-11 12:02:03 -0300

    se agregan botones de flitrado y header con barra de navegacion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import Barrita from "../Models/Barrita.js"
 import { RequestsAPI } from "../RequestsAPI.js";
-import { imprimir } from "../utils/helpers.js";
+import { imprimir, obtenerValorInput } from "../utils/helpers.js";
 
 
 const mostrarListaBarritas = (data) => {
@@ -8,8 +8,17 @@ const mostrarListaBarritas = (data) => {
     console.log("datos recibidos", data)
     imprimir("#lista-error", "")
 
-    const listadoBarritas = data.map((barrita) => new Barrita(barrita.id, barrita.sabor, barrita.tipo, barrita.precio, barrita.imagen).mostrarEnLista()).join("")
+    if (data.length === 0) {
+        // Si no hay barritas para mostrar, mostramos un mensaje adecuado
+        imprimir("#listado-barritas", "<p>No hay barritas disponibles para este filtro.</p>");
+        return;
+    }
 
+    const listadoBarritas = data
+        .map((barrita) =>
+            new Barrita(barrita.id, barrita.sabor, barrita.tipo, barrita.precio, barrita.imagen).mostrarEnLista()
+        )
+        .join("");
     imprimir("#listado-barritas", listadoBarritas);
 }
 
@@ -17,6 +26,22 @@ const mostrarError = (error) => {
     imprimir("#lista-error", error)
 }
 
-RequestsAPI.getBarritas()
-    .then(mostrarListaBarritas)
-    .catch(mostrarError)
+
+const obtenerBarritas = (filtroTipo = "") => {
+    // Si hay un tipo, se pasa el filtro a la solicitud
+    RequestsAPI.getBarritas({ filtroTipo })
+        .then(mostrarListaBarritas)
+        .catch(mostrarError);
+};
+obtenerBarritas();
+
+const agregarEventosDeFiltro = () => {
+    document.querySelectorAll(".tipos-barritas-buttons button").forEach((button) => {
+        button.addEventListener("click", () => {
+            const tipoSeleccionado = button.getAttribute("data-value");
+            obtenerBarritas(tipoSeleccionado);
+        });
+    });    
+};
+
+agregarEventosDeFiltro();
