43ce02a159b40b74c3612ed2eb66fe1331f26d50
Cata <127821154+catalinaAlba@users.noreply.github.com>
2024-12-12 20:33:14 -0300

    imgs

--- Models/Barrita.js
+++ Models/Barrita.js
@@ -17,6 +17,9 @@ export default class Barrita {
         return `
           <div class="card" id="${this.id}">
               <div class="card-content">
+                    <div>
+                        <img src="${this.imagen}"  class="card-image">
+                    </div>
                   <strong class="card-title">${this.sabor}</strong>
                   <p class="card-type">${this.tipo}</p>
                   <p class="card-price">$${this.precio.toFixed(2)}</p>
@@ -28,25 +31,25 @@ export default class Barrita {
 
     mostrarAmpliacion() {
         return `
-        <div class="layout">
-           
-            <section class="barrita-image">
-                <img src=" <!-- AGREGAR THIS.IMG -->" alt="Barrita x">
-            </section>
-
+            <div class="layout">
             
-            <section class="barrita-info">
-                <h1>Barrita de ${this.sabor}</h1>
-                <h3>${this.tipo.toUpperCase()}</h3>
-                <p class="barrita-description">
-                    Disfruta de la mejor barrita energética con ingredientes naturales y un sabor delicioso. Ideal para recargar energías antes o después de tu entrenamiento.
-                </p>
-                
-                <p class="barrita-price">$${this.precio.toFixed(2)}</p>
+                <div class="barrita-image">
+                    <img src="${this.imagen}"  class="card-image">
+                </div>
 
-                <button class="cta-btn" class="add-to-box">Añadir <!-- AGREGAR ICONO DE BOX --></button>
-            </section>
-        </div>
+                
+                <section class="barrita-info">
+                    <h1>Barrita de ${this.sabor}</h1>
+                    <h3>${this.tipo.toUpperCase()}</h3>
+                    <p class="barrita-description">
+                        Disfruta de la mejor barrita energética con ingredientes naturales y un sabor delicioso. Ideal para recargar energías antes o después de tu entrenamiento.
+                    </p>
+                    
+                    <p class="barrita-price">$${this.precio.toFixed(2)}</p>
+
+                    <button class="cta-btn" class="add-to-box">Añadir <!-- AGREGAR ICONO DE BOX --></button>
+                </section>
+            </div>
         `;
     }
 }
